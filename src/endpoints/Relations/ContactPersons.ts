cf36e8a6f7db81139b8baaccd3277811540bccb8
Ahsan Fazal <me@ahsanfazal.com>
2023-10-04 10:53:13 +0200

    Add `Relations.ContactPersons` endpoint

--- src/endpoints/Relations/ContactPersons.ts
+++ src/endpoints/Relations/ContactPersons.ts
@@ -1,3 +1,44 @@
 import { BaseEndpoint } from "../Base"
 
-export class ContactPersons extends BaseEndpoint {}
+export class ContactPersons extends BaseEndpoint {
+  public get = (relation: any) =>
+    this.httpClient.GET("/relations/{relation}/contactpersons", {
+      params: { path: { relation } }
+    })
+
+  public store = (relation: any) =>
+    this.httpClient.POST("/relations/{relation}/contactpersons", {
+      params: { path: { relation } }
+    })
+
+  public getById = (relation: any, contactperson: any) =>
+    this.httpClient.GET(
+      "/relations/{relation}/contactpersons/{contactperson}",
+      {
+        params: { path: { relation, contactperson } }
+      }
+    )
+
+  public updateById = (relation: any, contactperson: any) =>
+    this.httpClient.PUT("/relations/{relation}/contactperson/{contactperson}", {
+      params: { path: { relation, contactperson } }
+    })
+
+  public deleteById = (relation: any, contactperson: any) =>
+    this.httpClient.DELETE(
+      "/relations/{relation}/contactperson/{contactperson}",
+      {
+        params: { path: { relation, contactperson } }
+      }
+    )
+
+  public external = {
+    store: (relation: any, sourceRelation: any, externalId: any) =>
+      this.httpClient.POST(
+        "/relations/{relation}/contactpersons/external/{sourceRelation}/{externalId}",
+        {
+          params: { path: { relation, sourceRelation, externalId } }
+        }
+      )
+  }
+}
