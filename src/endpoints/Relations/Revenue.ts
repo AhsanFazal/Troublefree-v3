76fc2cc8e146e74d491aed24f192ff0fc6803df6
Ahsan Fazal <me@ahsanfazal.com>
2023-10-04 10:57:31 +0200

    Add `Relations.Revenue` endpoint

--- src/endpoints/Relations/Revenue.ts
+++ src/endpoints/Relations/Revenue.ts
@@ -1,3 +1,18 @@
 import { BaseEndpoint } from "../Base"
 
-export class Revenue extends BaseEndpoint {}
+export class Revenue extends BaseEndpoint {
+  public get = (query: any, relation: any) =>
+    this.httpClient.GET("/relations/{relation}/revenue", {
+      params: { query, path: { relation } }
+    })
+
+  public getByArticleGroup = (query: any, relation: any) =>
+    this.httpClient.GET("/relations/{relation}/revenue/by_article_group", {
+      params: { query, path: { relation } }
+    })
+
+  public getByMonth = (query: any, relation: any) =>
+    this.httpClient.GET("/relations/{relation}/revenue/by_month", {
+      params: { query, path: { relation } }
+    })
+}
